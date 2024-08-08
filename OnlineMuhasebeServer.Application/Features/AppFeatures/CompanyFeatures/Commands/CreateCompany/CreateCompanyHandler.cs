ceac58886e6029a61fb9ddd1e953ce98b62bb492
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-08 13:57:54 +0300

    cors

---
+++ OnlineMuhasebeServer.Application/Features/AppFeatures/CompanyFeatures/Commands/CreateCompany/CreateCompanyHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace OnlineMuhasebeServer.Application.Features.AppFeatures.CompanyFeatures.Commands.CreateCompany
+{
+    public sealed class CreateCompanyHandler : IRequestHandler<CreateCompanyRequest, CreateCompanyResponse>
+    {
+        public async Task<CreateCompanyResponse> Handle(CreateCompanyRequest request, CancellationToken cancellationToken)
+        {
+            return new();
+        }
+    }
+}
