2d624e9b7816ea50f8364e1e78e6d4d0d35e18fa
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-08 13:24:38 +0300

    Company db context and configurations created. Assembly references is given

---
+++ OnlineMuhasebeServer.Persistance/Constants/TableNames.cs
@@ -0,0 +1,9 @@
+﻿using OnlineMuhasebeServer.Domain.CompanyEntities;
+
+namespace OnlineMuhasebeServer.Persistance.Constants
+{
+    public static class TableNames
+    {
+        public static string UniformChartOfAccount = nameof(UniformChartOfAccount);
+    }
+}
--- OnlineMuhasebeServer.Persistance/Context/CompanyDbContext.cs
+++ OnlineMuhasebeServer.Persistance/Context/CompanyDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using OnlineMuhasebeServer.Domain.AppEntities;
 using System;
 using System.Collections.Generic;
@@ -11,37 +12,51 @@ namespace OnlineMuhasebeServer.Persistance.Context
     public sealed class CompanyDbContext : DbContext
     {
         private string ConnectionString = "";
-        private readonly AppDbContext _appDbContext;
 
-        public CompanyDbContext(string CompanyId, AppDbContext appDbContext)
+        public CompanyDbContext(string CompanyId, Company company = null)
         {
-            _appDbContext = appDbContext;
-            Company company = _appDbContext.Companies.Find(CompanyId);
-            if(company.UserId == "")
-                ConnectionString = $"" +
-                    $"Data Source={company.ServerName};" +
-                    $"Initial Catalog={company.DatabaseName};" +
-                    $"Integrated Security=True;" +
-                    $"Connect Timeout=30;" +
-                    $"Encrypt=True;Trust Server Certificate=True;" +
-                    $"Application Intent=ReadWrite" +
-                    $";Multi Subnet Failover=False";
-            else
-                ConnectionString = $"" +
-                    $"Data Source={company.ServerName};" +
-                    $"Initial Catalog={company.DatabaseName};" +
-                    $"User Id={company.UserId}; " +
-                    $"Password={company.Password} Integrated Security=True;" +
-                    $"Connect Timeout=30;" +
-                    $"Encrypt=True;Trust Server Certificate=True;" +
-                    $"Application Intent=ReadWrite;" +
-                    $"Multi Subnet Failover=False";
+            if(company != null)
+            {
+                if(company.UserId == "")
+                    ConnectionString = $"" +
+                        $"Data Source={company.ServerName};" +
+                        $"Initial Catalog={company.DatabaseName};" +
+                        $"Integrated Security=True;" +
+                        $"Connect Timeout=30;" +
+                        $"Encrypt=True;Trust Server Certificate=True;" +
+                        $"Application Intent=ReadWrite" +
+                        $";Multi Subnet Failover=False";
+                else
+                    ConnectionString = $"" +
+                        $"Data Source={company.ServerName};" +
+                        $"Initial Catalog={company.DatabaseName};" +
+                        $"User Id={company.UserId}; " +
+                        $"Password={company.Password} Integrated Security=True;" +
+                        $"Connect Timeout=30;" +
+                        $"Encrypt=True;Trust Server Certificate=True;" +
+                        $"Application Intent=ReadWrite;" +
+                        $"Multi Subnet Failover=False";
+            }
+           
 
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+
+        public class CompanyDbContextFactory : IDesignTimeDbContextFactory<CompanyDbContext>
+        {
+            private readonly AppDbContext dbContext;
+
+            public CompanyDbContext CreateDbContext(string[] args)
+            {
+                return new CompanyDbContext("");
+            }
         }
     }
 }
